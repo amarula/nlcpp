7854f112e77d525b4a3edd234c8735f0c0e4886c
Roman Smrž <roman@amarulasolutions.com>
2025-03-03 13:59:31 +0100

    Initial test setup with interface list
    Add test tool and configuration for erebos-tester framework, along with
    a sample test to list network interfaces.
    
    Change-Id: Ibe64e45a45d67ab2107e9c17a31f505b6a883e3d
    Signed-off-by: Roman Smrž <roman@amarulasolutions.com>

---
+++ src/test-tool.cpp
@@ -0,0 +1,117 @@
+#include <nlcpp.h>
+
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::function;
+using std::mutex;
+using std::ostringstream;
+using std::scoped_lock;
+using std::string;
+using std::vector;
+
+using namespace nl;
+
+namespace {
+
+mutex outputMutex;
+void printLine(const string & line)
+{
+	scoped_lock lock(outputMutex);
+	cout << line << endl;
+}
+
+struct Command
+{
+	string name;
+	function<void(const vector<string> &)> action;
+};
+
+void linkList(const vector<string> &)
+{
+	RouteCacheManager mngr;
+	auto rcache = mngr.linkCache();
+
+	for (const auto & link : rcache) {
+		ostringstream ss;
+		ss << "link-list-item " << link.ifindex();
+		if (const auto name = link.name())
+			ss << " " << name.value();
+		printLine(ss.str());
+	}
+	printLine("link-list-done");
+}
+
+vector<Command> commands = {
+	{ "link-list", linkList },
+};
+
+}
+
+int main(int argc, char * argv[])
+{
+	char * line = nullptr;
+	size_t size = 0;
+
+	if (argc > 1) {
+		vector<string> args;
+		for (int i = 2; i < argc; i++)
+			args.emplace_back(argv[i]);
+
+		for (const auto & cmd : commands) {
+			if (cmd.name == argv[1]) {
+				cmd.action(args);
+				return 0;
+			}
+		}
+
+		cerr << "Unknown command: '" << argv[1] << "'" << endl;
+		return 1;
+	}
+
+	while (getline(&line, &size, stdin) > 0) {
+		optional<string> command;
+		vector<string> args;
+
+		const char * last = line;
+		for (const char * cur = line;; cur++) {
+			if (isspace(*cur) || *cur == '\0') {
+				if (last < cur) {
+					if (!command)
+						command.emplace(last, cur);
+					else
+						args.emplace_back(last, cur);
+				}
+				last = cur + 1;
+
+				if (*cur == '\0')
+					break;
+			}
+		}
+
+		if (!command)
+			continue;
+
+		bool found = false;
+		for (const auto & cmd : commands) {
+			if (cmd.name == *command) {
+				found = true;
+				cmd.action(args);
+				break;
+			}
+		}
+
+		if (!found)
+			cerr << "Unknown command: '" << *command << "'" << endl;
+	}
+
+	free(line);
+	return 0;
+}
