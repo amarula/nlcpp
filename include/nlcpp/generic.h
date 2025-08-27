67c694a852ab580e56e47da4189119c17caa2e5c
Roman Smrž <roman@amarulasolutions.com>
2025-08-27 16:03:16 +0200

    Generic netlink message helper class
    Change-Id: Id1f4483353b3d703683bf244a69865561e7dc926
    Signed-off-by: Roman Smrž <roman@amarulasolutions.com>

--- include/nlcpp/generic.h
+++ include/nlcpp/generic.h
@@ -4,9 +4,15 @@
 
 #pragma once
 
+#include <nlcpp/message.h>
 #include <nlcpp/netlink.h>
 
+struct genlmsghdr;
+struct nlattr;
+
 namespace nl {
+
+class GenericNetlinkMessage;
 
 /// Socket opened for the generic netlink protocol
 class GenericSocket : protected Socket
@@ -17,10 +23,37 @@ public:
 	int resolve(const string & name);
 	int resolveGroup(const string & family, const string & group);
 
+	/// Send given message and wait for ACK or FINISH, calling given
+	/// callback for all valid replies.
+	void sendMessageSync(const GenericNetlinkMessage & message, function<void(GenericNetlinkMessage)> callback);
+
 	class Nl80211;
 	/// Interface for nl80211 netlink family. Returned object can be used
 	/// only as long as this GenericSocket object exists.
 	Nl80211 nl80211();
+};
+
+class GenericNetlinkMessage : public Message
+{
+public:
+	GenericNetlinkMessage(uint32_t port, uint32_t seq, int family,
+			int hdrlen, int flags, uint8_t cmd, uint8_t version);
+	explicit GenericNetlinkMessage(nl_msg *);
+
+	/// Get command number
+	unsigned int command() const;
+
+	/// Get version number
+	unsigned int version() const;
+
+	/// Get the raw generic netlink message header pointer
+	const genlmsghdr * getHeader() const;
+
+	/// Get the raw libnl pointer to attribute data
+	const nlattr * getAttrData(int hdrlen = 0) const;
+
+	/// Get length of message attributes
+	int getAttrLen(int hdrlen = 0) const;
 };
 
 }
--- src/generic.cpp
+++ src/generic.cpp
@@ -5,6 +5,8 @@
 
 namespace nl {
 
+using std::move;
+
 GenericSocket::GenericSocket():
 	Socket(NETLINK_GENERIC)
 {}
@@ -23,6 +25,49 @@ int GenericSocket::resolveGroup(const string & family, const string & group)
 	if (res < 0)
 		Exception::throwCode("genl_ctrl_resolve_grp failed", res);
 	return res;
+}
+
+void GenericSocket::sendMessageSync(const GenericNetlinkMessage & message, function<void(GenericNetlinkMessage)> callback)
+{
+	Socket::sendMessageSync(message, [callback = move(callback)](Message msg) {
+		callback(GenericNetlinkMessage(msg.get()));
+	});
+}
+
+
+GenericNetlinkMessage::GenericNetlinkMessage(uint32_t port, uint32_t seq, int family,
+		int hdrlen, int flags, uint8_t cmd, uint8_t version)
+{
+	genlmsg_put(get(), port, seq, family, hdrlen, flags, cmd, version);
+}
+
+GenericNetlinkMessage::GenericNetlinkMessage(nl_msg * msg):
+	Message(msg)
+{}
+
+unsigned int GenericNetlinkMessage::command() const
+{
+	return getHeader()->cmd;
+}
+
+unsigned int GenericNetlinkMessage::version() const
+{
+	return getHeader()->version;
+}
+
+const genlmsghdr * GenericNetlinkMessage::getHeader() const
+{
+	return static_cast<genlmsghdr *>(nlmsg_data(nlmsg_hdr(const_cast<nl_msg *>(get()))));
+}
+
+const nlattr * GenericNetlinkMessage::getAttrData(int hdrlen) const
+{
+	return genlmsg_attrdata(getHeader(), hdrlen);
+}
+
+int GenericNetlinkMessage::getAttrLen(int hdrlen) const
+{
+	return genlmsg_attrlen(getHeader(), hdrlen);
 }
 
 }
