e38bd1281f40e1bd6fc7e8d56cf44c78dae412d8
Roman Smrž <roman@amarulasolutions.com>
2025-08-27 16:27:36 +0200

    Get interface command in nl80211 netlink family
    Change-Id: I715dee26ba72fdc757d8bf9e1c3219c190551ac6
    Changelog: Added methods wrapping GET_INTERFACE command in nl80211 generic netlink family
    Signed-off-by: Roman Smrž <roman@amarulasolutions.com>

--- include/nlcpp/nl80211.h
+++ include/nlcpp/nl80211.h
@@ -6,14 +6,59 @@
 
 #include <nlcpp/generic.h>
 
+#include <linux/if_ether.h>
+
+#include <array>
+#include <optional>
+#include <vector>
+
 namespace nl {
+
+using std::array;
+using std::optional;
+using std::vector;
+
+namespace nl80211 {
+
+enum class InterfaceType
+{
+	UNSPECIFIED,
+	ADHOC,
+	STATION,
+	AP,
+	AP_VLAN,
+	WDS,
+	MONITOR,
+	MESH_POINT,
+	P2P_CLIENT,
+	P2P_GO,
+	P2P_DEVICE,
+	OCB,
+	NAN_,
+};
+
+struct InterfaceConfiguration
+{
+	uint32_t ifindex;
+	uint32_t wiphy;
+	InterfaceType iftype;
+	array<uint8_t, ETH_ALEN> mac;
+	bool use4addr;
+};
+
+}
 
 class GenericSocket::Nl80211
 {
 public:
 	explicit Nl80211(GenericSocket & genl);
 
+	optional<nl80211::InterfaceConfiguration> getInterface(uint32_t ifindex);
+	vector<nl80211::InterfaceConfiguration> getInterfaces();
+
 private:
+	void parseInterfaceConfiguration(nl80211::InterfaceConfiguration &, const GenericNetlinkMessage &) const;
+
 	GenericSocket & genl;
 	const int family;
 };
--- src/nl80211.cpp
+++ src/nl80211.cpp
@@ -1,8 +1,17 @@
 #include <nlcpp/nl80211.h>
 
 #include <linux/nl80211.h>
+#include <netlink/genl/genl.h>
+
+#include <nlcpp/message.h>
+
+#include <array>
 
 namespace nl {
+
+using namespace nl80211;
+
+using std::array;
 
 GenericSocket::Nl80211 GenericSocket::nl80211()
 {
@@ -13,6 +22,58 @@ GenericSocket::Nl80211::Nl80211(GenericSocket & genl_)
 	: genl(genl_)
 	, family(genl_.resolve(NL80211_GENL_NAME))
 {
+}
+
+optional<InterfaceConfiguration> GenericSocket::Nl80211::getInterface(uint32_t ifindex)
+{
+	GenericNetlinkMessage msg(0, 0, family, 0, 0, NL80211_CMD_GET_INTERFACE, 0);
+	msg.put_u32(NL80211_ATTR_IFINDEX, ifindex);
+
+	optional<InterfaceConfiguration> result;
+
+	genl.sendMessageSync(msg, [&](GenericNetlinkMessage reply) {
+		result = InterfaceConfiguration {};
+		parseInterfaceConfiguration(*result, reply);
+	});
+
+	return result;
+}
+
+vector<InterfaceConfiguration> GenericSocket::Nl80211::getInterfaces()
+{
+	GenericNetlinkMessage msg(0, 0, family, 0, NLM_F_DUMP, NL80211_CMD_GET_INTERFACE, 0);
+
+	vector<InterfaceConfiguration> result;
+
+	genl.sendMessageSync(msg, [&](GenericNetlinkMessage reply) {
+		result.emplace_back();
+		parseInterfaceConfiguration(result.back(), reply);
+	});
+
+	return result;
+}
+
+void GenericSocket::Nl80211::parseInterfaceConfiguration(InterfaceConfiguration & interface, const GenericNetlinkMessage & message) const
+{
+	array<nlattr *, NL80211_ATTR_MAX + 1> tb_msg {};
+	nla_parse(tb_msg.data(), tb_msg.size() - 1,
+			const_cast<nlattr*>(message.getAttrData()),
+			message.getAttrLen(), nullptr);
+
+	if (tb_msg[NL80211_ATTR_IFINDEX])
+		interface.ifindex = nla_get_u32(tb_msg[NL80211_ATTR_IFINDEX]);
+
+	if (tb_msg[NL80211_ATTR_WIPHY])
+		interface.wiphy = nla_get_u32(tb_msg[NL80211_ATTR_WIPHY]);
+
+	if (tb_msg[NL80211_ATTR_IFTYPE])
+		interface.iftype = static_cast<InterfaceType>(nla_get_u32(tb_msg[NL80211_ATTR_IFTYPE]));
+
+	if (tb_msg[NL80211_ATTR_MAC])
+		memcpy(interface.mac.data(), nla_data(tb_msg[NL80211_ATTR_MAC]), ETH_ALEN);
+
+	if (tb_msg[NL80211_ATTR_4ADDR])
+		interface.use4addr = nla_get_u8(tb_msg[NL80211_ATTR_4ADDR]);
 }
 
 }
